4fa93530109fe9b1a570fe2deb54c317664ce7f9
Kenzo Nirasawa <kenzo_nirasawa@r.recruit.co.jp>
2016-03-29 14:36:03 +0900

    first commit

---
+++ PlaygroundPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaygroundPrototype
+//
+//  Created by 韮澤賢三 on 2016/03/29.
+//  Copyright © 2016年 KenzoNirasawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
